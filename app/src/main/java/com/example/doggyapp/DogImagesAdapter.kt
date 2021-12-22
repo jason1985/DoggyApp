f816b2356a412b41b37444a4ffba102180ab192b
jason <10107412+jason1985@users.noreply.github.com>
2021-12-22 14:41:47 -0800

    add: global state favList and star imageView to indicate favorited dogs

--- app/src/main/java/com/example/doggyapp/DogImagesAdapter.kt
+++ app/src/main/java/com/example/doggyapp/DogImagesAdapter.kt
@@ -1,5 +1,7 @@
 package com.example.doggyapp
 
+import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,7 +13,6 @@ import com.bumptech.glide.Glide
 class DogImagesAdapter(private val dogImages: ArrayList<Dog>) : RecyclerView.Adapter<DogImagesAdapter.ViewHolder>() {
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val dogImage: ImageView = itemView.findViewById(R.id.iv_dog_image)
-//        val dogUrl: TextView = itemView.findViewById(R.id.tv_url)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -20,15 +21,36 @@ class DogImagesAdapter(private val dogImages: ArrayList<Dog>) : RecyclerView.Ada
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val favOff: ImageView = holder.itemView.findViewById(R.id.iv_fav_off)
+        val favOn: ImageView = holder.itemView.findViewById(R.id.iv_fav_on)
+
+        if(MyApplication.getFavList().contains(dogImages[position].breed)) {
+            favOff.visibility = View.INVISIBLE
+            favOn.visibility = View.VISIBLE
+        } else{
+            favOff.visibility = View.VISIBLE
+            favOn.visibility = View.INVISIBLE
+        }
+
+        holder.itemView.setOnClickListener{
+            if(MyApplication.getFavList().contains(dogImages[position].breed)) {
+                MyApplication.remove(dogImages[position].breed)
+                favOff.visibility = View.VISIBLE
+                favOn.visibility = View.INVISIBLE
+            } else{
+                MyApplication.add(dogImages[position].breed)
+                favOff.visibility = View.INVISIBLE
+                favOn.visibility = View.VISIBLE
+            }
+            Log.d("Jason",MyApplication.getFavList().toString())
+        }
         val dog = dogImages[position]
         Glide.with(holder.itemView.context)
             .load(dog.breed)
             .into(holder.dogImage)
-//        holder.dogUrl.text = dog.breed
     }
 
     override fun getItemCount(): Int {
         return dogImages.size
     }
-
 }
